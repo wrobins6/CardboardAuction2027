81d7bf3f59d8955cff5945066bccc617b78d780d
craigslisttrashman <fintanneff@gmail.com>
2022-10-31 16:55:24 -0400

    Time zone fix
    Time zone fix

--- prototype1/settings.py
+++ prototype1/settings.py
@@ -108,11 +108,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'EST'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
